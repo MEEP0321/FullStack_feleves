45847a66d6085eedee29281469b1838ae120e1a8
MEEP <polyak.attila56@gmail.com>
2025-04-20 17:23:25 +0200

    Random háttér szín generáló function kész

--- FRONTEND/main.js
+++ FRONTEND/main.js
@@ -63,8 +63,20 @@ function generateCards(){
     })
     .then(resp => resp.json())
     .then(data => {
-        console.log(data)
+ 
     });
+}
+
+
+function random_bg_color() {
+    // Generate random values for red, green, and blue components between 0 and 255.
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    // Construct the RGB color string.
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    
+    return bgColor
 }
 
 
